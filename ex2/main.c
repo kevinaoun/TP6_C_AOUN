c2409102c70776ecea9877514016e327499326c6
kevinaoun <kevinaoun>
2023-12-22 10:42:25 +0100

    Compte Rendu TP6 Langage C

--- ex2/main.c
+++ ex2/main.c
@@ -26,9 +26,69 @@ TreeNode *newNodeAuto(int val){
     return Node;
 }
 
-TreeNode *insert(TreeNode *tree){
+TreeNode *insert(TreeNode *tree) {  // SANS RECURSIVITE
     printf("Element à ajouter : ");
     TreeNode *initiale = tree;
     if (tree == NULL) return newNode();
-    
+
+    TreeNode *node = newNode();
+    while (node->val < tree->val){
+        if (tree->left == NULL) {
+            tree->left = node;
+            return initiale;
+        }
+        tree = tree->left;
+    }
+    while (node->val >= tree->val){
+        if (tree->right == NULL) {
+            tree->right = node;
+            return initiale;
+        }
+        tree = tree->right;
+    }
+
+    printf("MESSAGE ERREUR\n");
+    return initiale;
+}
+
+void inorder(TreeNode *tree) {     // SANS RECURSIVITE
+    while (tree->left != NULL) {
+        tree = tree->left;
+    }
+    while (tree->right != NULL) {
+        printf("%d ",tree->val);
+        tree = tree->right;
+    }
+    printf("%d",tree->val);
+    if (tree->left != NULL) printf("\nDernier enfant à droite\n");
+    else printf("\nArrivé à la feuille\n");
+}
+
+void freeTree(TreeNode *tree) {
+    TreeNode *temp;
+    while (tree != NULL) {
+        if (tree->left != NULL) {
+            temp = tree;
+            tree = tree->left;
+            free(temp);
+        }
+        else if (tree->right != NULL) {
+            temp = tree;
+            tree = tree->right;
+            free(temp);
+        }
+    }
+    printf("L'espace alloué a bien été libéré\n");
+}
+
+int main() {
+    TreeNode *tree = NULL;
+    printf("Valeur de la racine : ");
+    tree = newNode(tree);
+    insert(tree);
+    insert(tree);
+    insert(tree);
+    insert(tree);
+    inorder(tree);
+    freeTree(tree);
 }
