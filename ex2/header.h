712b11529d59a149a8ab18d85d3e2d7359a1eec6
kevinaoun <kevinaoun>
2023-12-22 20:33:17 +0100

    Compte Rendu TP6 Langage C

---
+++ ex2/header.h
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct TreeNode_{
+    int val;
+    struct TreeNode_ *left;
+    struct TreeNode_ *right;
+}TreeNode;
+
+// Ajouter un noeud à l'arbre dont la valeur est saisie par l'utilisateur
+TreeNode *newNode();
+// Ajouter un noeud à l'arbre dont la valeur est prise en paramètre de fonction
+TreeNode *newNodeAuto(int val);
+
+// Inserer une valeur (prise en paramètre) à sa place dans l'arbre
+void insert(TreeNode *tree, int val);
+// Inserer une valeur (saisie par l'utilisateur) à sa place dans l'arbre
+void insertAuto(TreeNode *tree);
+
+// Afficher les valeurs de l'arbre dans l'ordre croissant
+void inorder(TreeNode *tree);
+
+// Libérer l'espace alloué pour l'arbre
+void freeTree(TreeNode *tree);
--- ex2/main.c
+++ ex2/main.c
@@ -1,88 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-typedef struct TreeNode_{
-    int val;
-    struct TreeNode_ *left;
-    struct TreeNode_ *right;
-}TreeNode;
-
-TreeNode *newNode(){
-    TreeNode *Node = malloc(sizeof(TreeNode));
-    int val;
-    //printf("Rentrer une valeur : ");
-    scanf("%d", &val);
-    Node->val=val;
-    Node->left=NULL;
-    Node->right=NULL;
-    return Node;
-}
-
-TreeNode *newNodeAuto(int val){
-    TreeNode *Node = malloc(sizeof(TreeNode));
-    Node->val=val;
-    Node->left=NULL;
-    Node->right=NULL;
-    return Node;
-}
-
-void insert(TreeNode *tree, int val) {
-    if (val < tree->val) {
-        if (tree->left == NULL) tree->left = newNodeAuto(val);
-        else insert(tree->left, val);
-    }
-    else if (val >= tree->val) {
-        if (tree->right == NULL) tree->right = newNodeAuto(val);
-        else insert(tree->right, val);
-    }
-    else {
-        printf("Arbre vide\n");
-        tree = newNodeAuto(val);
-    }
-}
-
-void inorder(TreeNode *tree) {
-
-    // if (tree == NULL) {
-    //     printf("L'arbre est vide\n") ;
-    //     return;
-    // }
-    // if (tree->left != NULL) {
-    //     inorder(tree->left);
-    //     if (tree->right != NULL) {
-    //         inorder(tree->right);
-    //     }
-    // }
-    // else {  // if (tree->left == NULL)
-    //     if (tree->right != NULL) {
-    //         inorder(tree->right);
-    //     }
-    // }
-    // printf("%d ",tree->val);
-
-    if (tree != NULL) {
-        inorder(tree->left);
-        printf("%d ",tree->val);
-        inorder(tree->right);
-    }
-
-}
-
-void freeTree(TreeNode *tree) {
-    TreeNode *temp = tree;
-    if (temp != NULL) {
-        freeTree(tree->left);
-        freeTree(tree->right);
-        free(temp);
-    }
-}
+#include "header.h"
 
 int main() {
+
     TreeNode *tree = NULL;
     // printf("Valeur de la racine : ");
-    // tree = newNodeAuto(101);
+    // tree = newNode();    // Saisir la valeur de la racine soit-même
+    tree = newNodeAuto(101);
     
-    insert(tree,101);
     insert(tree,8);
     insert(tree,14);
     insert(tree,129);
@@ -90,8 +16,12 @@ int main() {
     insert(tree,99);
     insert(tree,13);
     insert(tree,55);
+    
+    // insertAuto(tree);
+    // insertAuto(tree);
 
     inorder(tree);
     printf("\n");
     freeTree(tree);
+    
 }
